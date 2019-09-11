9a7938569dc8548f1e0da56c85b9edacae5f995c
ayrbox <srdx7@hotmail.com>
2019-09-11 13:34:28 +0100

    Add stripped off for non-working workspace state

--- src/components/CalendarCell/CalendarCell.tsx
+++ src/components/CalendarCell/CalendarCell.tsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { style } from 'typestyle';
 import { color } from 'csx';
+import clsx from 'clsx';
 
 import { WORKSPACES } from '../../constants';
 import CalendarContext from '../Calendar/CalendarContext';
@@ -13,6 +14,10 @@ interface CalendarCellProps {
   colSpan: number;
 }
 
+const strippedOff = style({
+  backgroundImage: `url("data:image/svg+xml,%3Csvg width='40' height='40' viewBox='0 0 40 40' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='%23f9f9fa' fill-opacity='1' fill-rule='evenodd'%3E%3Cpath d='M0 40L40 0H20L0 20M40 40V20L20 40'/%3E%3C/g%3E%3C/svg%3E")`,
+});
+
 export const CalendarCell: React.FC<CalendarCellProps> = ({
   employeeName,
   dateKey,
@@ -21,7 +26,7 @@ export const CalendarCell: React.FC<CalendarCellProps> = ({
   colSpan,
 }: CalendarCellProps) => {
   const { defaultWorkspace } = useContext(CalendarContext);
-  const { label, color: backColor } = WORKSPACES.find(w => w.key === workspace) || defaultWorkspace;
+  const { label, color: backColor, state } = WORKSPACES.find(w => w.key === workspace) || defaultWorkspace;
 
   const tdStyle = style({
     padding: '0 !important', // todo: remove important
@@ -36,9 +41,11 @@ export const CalendarCell: React.FC<CalendarCellProps> = ({
     borderLeft: `5px solid ${bgColor.darken('20%').toHexString()}`,
   });
 
+  const textClass = clsx(spaceStyle, state === 0 && strippedOff);
+
   return (
     <td className={tdStyle} colSpan={colSpan}>
-      <span className={spaceStyle}>{label}</span>
+      <span className={textClass}>{label}</span>
     </td>
   );
 };

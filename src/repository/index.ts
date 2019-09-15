d1f370aeefad4f969e48b4f6427f1acb96c04f99
Sabin Raj DANGOL <sawbean.raz@googlemail.com>
2019-09-15 22:51:47 +0100

    Update firebase pacakge import

--- src/repository/index.ts
+++ src/repository/index.ts
@@ -1,4 +1,5 @@
-import firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/database';
 import config from '../config/firebase.config';
 
 firebase.initializeApp(config);

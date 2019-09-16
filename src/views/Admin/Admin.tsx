7162ba777ea8761f53b0d270f615c4f24db4d5be
Sabin Raj DANGOL <sawbean.raz@googlemail.com>
2019-09-16 01:08:57 +0100

    update admin view header

--- src/views/Admin/Admin.tsx
+++ src/views/Admin/Admin.tsx
@@ -1,9 +1,26 @@
 import React, { FC, useContext } from 'react';
+import { style } from 'typestyle';
 
 import UserContext from '../../UserContext';
 import { RouteComponentProps } from 'react-router';
 
 type AdminViewProps = RouteComponentProps;
+
+const masthead = style({
+  background: '#000',
+  color: '#fff',
+  padding: '32px',
+});
+
+const mastheadInner = style({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignContent: 'center',
+  background: '#000',
+  color: '#fff',
+  padding: '32px',
+  margin: 'auto',
+});
 
 const AdminView: FC<AdminViewProps> = ({ history }: AdminViewProps) => {
   const { user, logout } = useContext(UserContext);
@@ -14,7 +31,17 @@ const AdminView: FC<AdminViewProps> = ({ history }: AdminViewProps) => {
   }
 
   return (
+
     <div>
+      <header className={`${masthead} mb-auto`}>
+        <div className={mastheadInner}>
+          <h3 className="masthead-brand">Workspace</h3>
+          <nav className="nav nav-masthead justify-content-center">
+            <div className="nav-link">{user.email}</div>
+            <a className="nav-link" onClick={logout}>Logout</a>
+          </nav>
+        </div>
+      </header>
       <h1>Admin View: {user.email}</h1>
       <pre>
         {JSON.stringify(user, null, 2)}
